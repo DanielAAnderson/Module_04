4f01da4246808b7998506a5638349cefb4fc2cb0
DanielAAnderson <166192257+DanielAAnderson@users.noreply.github.com>
2024-11-17 21:32:50 -0500

    Add files via upload

---
+++ zoo.py
@@ -0,0 +1,10 @@
+"""
+This program defines a function called hours
+that prints the string "Open 9-5 daily"
+Daniel Anderson
+Version 1.0
+11/12/2024
+"""
+
+def hours():
+    print('Open 9-5 daily')
